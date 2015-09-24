74ffda0b59709549fa2705ec4d02e7f74045c927
Darion Cassel <darion.cassel@gmail.com>
2015-09-24 13:53:47 -0700

    Autocomplete with regex

---
+++ autocomplete.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+
+def main(str):
+    with open('/usr/share/dict/words') as file:
+        words = file.readlines()
+        regex = '%s[a-z]*' % str
+        search = re.compile(regex)
+        for word in words:
+            if search.match(word):
+                print(word)
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    main(arg)
