d3e4fa5d0edb6c67096a33cdf285d429154e31d3
Darion Cassel <darioncassel@Darions-MacBook-2.local>
2015-09-24 11:25:04 -0700

    Added Pride and Prejudice corpus and random word blanking

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,18 @@
 import sys
 import random
 
-text = open("paragraphs.txt").read()
-arr = text.split("\n")
+
+def main():
+    text = open("paragraphs.txt").read()
+    paragraphs = text.split("\n")
+    num = random.randint(0, len(paragraphs))
+    paragraph = text.split("\n")[num]
+    words = paragraph.split(' ')
+    for x in range(10):
+        num = random.randint(0, len(words)-1)
+        words[num] = "_____"
+    return ' '.join(words)
 
 
 if __name__ == "__main__":
-    print(len(arr))
+    print(main())
