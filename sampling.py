7ee2f47cc5ca1824fc6f72ce80653662aaee60fc
Darion Cassel <darion.cassel@gmail.com>
2015-09-25 12:47:54 -0700

    Write efficient weighted select

--- sampling.py
+++ sampling.py
@@ -3,14 +3,22 @@ import random
 from frequency import histogram
 
 
-def main(hist):
+def unweighted_select(hist):
     num = random.randint(0, len(hist.keys())-1)
     print(len(hist.keys()))
     return list(hist.keys())[num]
 
 
+def weighted_select(hist):
+    length = sum(hist[x] for x in hist.keys())
+    num = random.randint(0, length)
+    for x in hist:
+        num = num - hist[x]
+        if num <= 0:
+            return x
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     text = ' '.join(args)
     hist = histogram(text)
-    print(main(hist))
+    print(weighted_select(hist))
