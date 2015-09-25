7efc12aecad8c7b9aea28d9883825bf21d177747
Darion Cassel <darion.cassel@gmail.com>
2015-09-25 11:37:55 -0700

    Update main to accept histogram argument

--- sampling.py
+++ sampling.py
@@ -3,13 +3,14 @@ import random
 from frequency import histogram
 
 
-def main(args):
-    text = ' '.join(args)
-    hist = histogram(text)
+def main(hist):
     num = random.randint(0, len(hist.keys())-1)
+    print(len(hist.keys()))
     return list(hist.keys())[num]
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    print(main(args))
+    text = ' '.join(args)
+    hist = histogram(text)
+    print(main(hist))
