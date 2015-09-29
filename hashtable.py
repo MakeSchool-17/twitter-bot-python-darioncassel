0237fcb8728b16e3b9c1445c67f04a25502a5ab0
Darion Cassel <darion.cassel@gmail.com>
2015-09-29 15:06:23 -0700

    Added analysis and benchmark

--- hashtable.py
+++ hashtable.py
@@ -1,49 +1,141 @@
 class HashTable:
 
-    keys = []
-    vals = []
-    length = 0
-
     def __init__(self, array):
+        self.keys = []
+        self.vals = []
+        self.length = 0
         self.length = len(array)
         self.vals = [None] * self.length
         for key in array:
-            if self.get(key):
+            if key in self.getKeys():
                 self.update(key, self.get(key)+1)
             else:
                 self.set(key, 1)
 
+    # returns val of key
     def get(self, key):
         index = self.index(key)
-        if index < len(self.vals):
-            return self.vals[index]
+        return self.vals[index].find(key).val
 
     def set(self, key, val):
         index = self.index(key)
-        self.vals[index] = val
-        self.keys.append(key)
+        if not self.vals[index]:
+            self.vals[index] = LinkedList()
+        self.vals[index].append(key)
+        if key not in self.keys:
+            self.keys.append(key)
 
     def update(self, key, val):
-        self.set(key, val)
+        index = self.index(key)
+        self.vals[index].update(key, val)
 
     def getKeys(self):
-        return self.keys()
+        return self.keys
 
     def getVals(self):
-        return self.vals()
+        array = []
+        for linkedlist in self.vals:
+            if linkedlist:
+                for node in linkedlist:
+                    if node:
+                        array.append(node.val)
+        return array
 
-    # Generates array index from key
+    # Generates vals index from key
     # params: length of key array, key string
     # (int, str) -> int
     def index(self, key):
-        return self.hash_func(key) % self.length
+        return hash(key) % self.length
 
-    def hash_func(self, key):
-        sum = 0
-        if len(key) > 1:
-            chars = key.split("")
-            for char in chars:
-                sum += ord(char)
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # appends node to end of list
+    def append(self, key):
+        node = Node(key)
+        # head and last node
+        if self.head and self.tail:
+            self.tail.next_node = node
+            self.tail = node
+        # head but no last node
+        elif self.head:
+            self.head.next_node = node
+            self.tail = node
+        # no head
         else:
-            sum += ord(key)
-        return sum
+            node.next_node = self.tail
+            self.head = node
+
+    # returns node given data
+    def find(self, data):
+        index = 0
+        node = self.head
+        while index < len(self):
+            if node.data == data:
+                return node
+            if node.next_node:
+                node = node.next_node
+                index += 1
+
+    # sets val of existing key
+    def update(self, key, val):
+        self.find(key).val = val
+
+    # remove node at index
+    def remove(self, index):
+        current_node = self[index]
+        if current_node == self.head:
+            self.head = current_node.next_node
+            current_node = None
+        else:
+            previous_node = self[index-1]
+            previous_node.next_node = current_node.next_node
+            del current_node
+
+    # required for iterable
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next_node
+
+    # required for iterable
+    def __getitem__(self, index):
+        counter = 0
+        node = self.head
+        while counter < index:
+            if node.next_node:
+                node = node.next_node
+                counter += 1
+            else:
+                print("Error: Index out of bounds")
+                return None
+        return node
+
+    # define for len() to work
+    def __len__(self):
+        counter = 0
+        node = self.head
+        while node:
+            counter += 1
+            node = node.next_node
+        return counter
+
+    # print the list
+    def __str__(self):
+        nodes = "["
+        for node in self:
+            nodes += "[{}, {}]".format(node.data, node.val)
+        return nodes + "]"
+
+
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.val = 1
+        self.next_node = None
--- histogram_tester.py
+++ histogram_tester.py
@@ -4,6 +4,7 @@ from flatassociativearray import FlatAssociativeArray
 from sortedsinglylinkedlist import SortedSinglyLinkedList
 from doublylinkedlist import DoublyLinkedList
 from binarysearchtree import BinarySearchTree
+from hashtable import HashTable
 
 
 # analysis: O(n), benchmark: O(n)
@@ -27,6 +28,12 @@ def test_dll(word, words):
 # analysis: O(log(n)), benchmark: O(log(n))
 def test_bst(word, words):
     frequency = words.binary_search(words.root_node, word)
+    return frequency
+
+
+# analysis: O(1), benchmark: O(n)
+def test_hashtable(word, words):
+    frequency = words.get(word)
     return frequency
 
 
@@ -55,36 +62,52 @@ def benchmark(struct, function, size):
                                                      struct.__name__,
                                                      size)
     timer = timeit.Timer(test, setup=setup)
-    num = 0
-    sum = 0
-    for x in range(100):
-        sum += timer.timeit(number=1)
-        num += 1
-    return sum/num
+    return timer.timeit(number=1)
 
 if __name__ == "__main__":
 
     # FAA 100: 0.027061045599912178
     print("FAA 100: " +
           str(benchmark(FlatAssociativeArray, test_tuple_list, 100)))
-    # FAA 1000: 0.11650251239971113
+    # FAA 1000: 0.12729319000209216
     print("FAA 1000: " +
           str(benchmark(FlatAssociativeArray, test_tuple_list, 1000)))
+    # FAA 10000: 8.47697632299969
+    print("FAA 10000: " +
+          str(benchmark(FlatAssociativeArray, test_tuple_list, 10000)))
     # SSLL 100: 0.02705211373995553
     print("SSLL 100: " +
           str(benchmark(SortedSinglyLinkedList, test_ssll, 100)))
-    # SSLL 1000: 0.11500938755001698
+    # SSLL 1000: 0.1555812309961766
     print("SSLL 1000: " +
           str(benchmark(SortedSinglyLinkedList, test_ssll, 1000)))
+    # SSLL 10000: 14.267478120993474
+    print("SSLL 10000: " +
+          str(benchmark(SortedSinglyLinkedList, test_ssll, 10000)))
     # DLL 100: 0.0305517836299623
     print("DLL 100: " +
           str(benchmark(DoublyLinkedList, test_dll, 100)))
-    # DLL 1000: 0.10661697606989037
+    # DLL 1000: 0.11440823299926706
     print("DLL 1000: " +
           str(benchmark(DoublyLinkedList, test_dll, 1000)))
+    # DLL 10000: 3.3517625449894695
+    print("DLL 10000: " +
+          str(benchmark(DoublyLinkedList, test_dll, 10000)))
     # BST 100: 0.027463051750310115
     print("BST 100: " +
           str(benchmark(BinarySearchTree, test_bst, 100)))
-    # BST 1000: 0.031981624280088
+    # BST 1000: 0.03592572700290475
     print("BST 1000: " +
           str(benchmark(BinarySearchTree, test_bst, 1000)))
+    # BST 10000: 0.11101376298756804
+    print("BST 10000: " +
+          str(benchmark(BinarySearchTree, test_bst, 10000)))
+    # HT 100: 0.026284409992513247
+    print("HT 100: " +
+          str(benchmark(HashTable, test_hashtable, 100)))
+    # HT 1000: 0.050136938007199205
+    print("HT 1000: " +
+          str(benchmark(HashTable, test_hashtable, 1000)))
+    # HT 10000: 1.7139951350109186
+    print("HT 10000: " +
+          str(benchmark(HashTable, test_hashtable, 10000)))
