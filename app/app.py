6b18b0291be10ff326e34f500749d50257b63179
Darion Cassel <darion.cassel@gmail.com>
2015-10-03 18:31:33 -0700

    Create Heroku config

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import markovmodel_runner
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def run_model():
     return markovmodel_runner.run(5, 4, 200)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
